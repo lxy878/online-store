d3bf98c2b82c7584dccf79d0f40ab14a6eaa0738
lxy878 <lxy878@msn.cn>
2020-12-18 10:08:58 -0500

    add users server side

---
+++ os_backend/app/controllers/users_controller.rb
@@ -0,0 +1,17 @@
+class UsersController < ApplicationController
+
+  # def index
+  #   users = User.all
+  #   render json: users.to_json()
+  # end
+
+  def create
+  end
+
+  def edit
+    # load current user
+  end
+
+  def update
+  end
+end
--- os_backend/config/routes.rb
+++ os_backend/config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post 'login' => 'users#create'
+  get 'users/:id' => 'users#edit'
+  post 'users/:id' => 'users#update'
+  # get 'users' => 'users#index'
 end
