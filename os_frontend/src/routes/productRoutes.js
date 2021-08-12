ab4753e996beb6851f2e797750baec2c89eb70ba
lxy878 <lxy878@yahoo.com>
2021-08-12 14:57:59 -0400

    UNDO "Add new design for products page"
    This reverts commit 8529e1a7c4e495201da578015bce5a394097ade2.

--- os_frontend/src/routes/productRoutes.js
+++ os_frontend/src/routes/productRoutes.js
@@ -1,11 +1,9 @@
 import React from 'react'
 import {Route} from "react-router-dom";
-// import ProductsContainer from '../containers/productsContainer'
-import ProductsContainerNew from '../containers/productsContainerNew'
+import ProductsContainer from '../containers/productsContainer'
 
 export default function routes({reload}) {
     return (<>
-        {/* <Route path="/products" render={routerProps=> <ProductsContainer {...routerProps} reload={reload}/>}/> */}
-        <Route path="/products" render={routerProps=> <ProductsContainerNew {...routerProps} reload={reload}/>}/>
+        <Route path="/products" render={routerProps=> <ProductsContainer {...routerProps} reload={reload}/>}/>
     </>);
 }
